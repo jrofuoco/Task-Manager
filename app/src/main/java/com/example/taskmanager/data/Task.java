970c00e6752c32e58105375b5acd62e7ba8e3c5d
Fuoco <joshuaorlina08@gmail.com>
2026-02-24 16:50:15 +0800

    Dialog Fragment added

--- app/src/main/java/com/example/taskmanager/data/Task.java
+++ app/src/main/java/com/example/taskmanager/data/Task.java
@@ -6,57 +6,17 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "tasks")
 public class Task {
     @PrimaryKey(autoGenerate = true)
-    private int id;
+    public int id;
 
-    private String title;
-    private String description;
-    private String time;
-    private String date;
+    public String title;
+    public String description;
+    public String time;
+    public String date;
 
     public Task(String title, String description, String time, String date) {
         this.title = title;
         this.description = description;
         this.time = time;
-        this.date = date;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
         this.date = date;
     }
 }
